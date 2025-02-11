526cbb0fd0ae34b5dd1abc2b1d5ad2ee0cd5ba89
Atharvaud29 <atharva.deshmukh20ad@gmail.com>
2025-02-11 09:58:00 +0530

    Docker ci cd yaml file is ready

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Word!"
+    return "Hello World!"
 
-if __name__=="__name__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
